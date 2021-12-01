940d3534caa68ec6f9f64b1c6087b985e88561a0
Tony Walsh <kilnageer@gmail.com>
2021-12-01 23:30:05 +0000

    Made code monitor Front Bedroom TRV

--- mqttTest/src/main/java/mqttTest/App.java
+++ mqttTest/src/main/java/mqttTest/App.java
@@ -6,6 +6,14 @@ import java.util.concurrent.*;
 
 public class App {
 
+	static String[] pollCommand = { "cmnd/tasmota_zbbridge_0576A9/status", "" };
+	static Integer pollCounter = 0; // incremented every poll
+	static String pollCounterTopic = "Tola/status/pollCounter";
+	
+	public static void incrementPollCounter() {
+    	pollCounter = pollCounter + 1;
+	}
+	
     public static void main(String[] args) throws Exception {
         int qos             = 2;
         String broker       = Secrets.getBroker();
@@ -22,73 +30,80 @@ public class App {
         options.setUserName( username );
         options.setPassword( password.toCharArray() );
         client.connect(options);
-        
-        
-    	MqttMessage message = new MqttMessage();
-    	message.setPayload("Hello world from Java".getBytes());
-    	client.publish("iot_data", message);
-    	//client.disconnect();
-    	/*
-        MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
-        MqttConnectOptions connOpts = new MqttConnectOptions();
-        connOpts.setCleanSession(true);
-        System.out.println("Connecting to broker: "+broker);
-        sampleClient.connect(connOpts);
-        System.out.println("Connected");
-        System.out.println("Publishing message: "+content);
-        MqttMessage message = new MqttMessage(content.getBytes());
-        message.setQos(qos);
-        sampleClient.publish(topic, message);
-        System.out.println("Message published");
-        sampleClient.disconnect();
-        */
-        //System.out.println("Disconnected");
-        //System.exit(0);
-
         System.out.println("The device has been registered successfully!");
 
+    	System.out.println("pollCounter:"+pollCounter);
+    	
         // listen for operations
-        client.subscribe("s/us", new IMqttMessageListener() {
+    	client.subscribe("stat/tasmota_zbbridge_0576A9/STATUS", new IMqttMessageListener() {
             public void messageArrived (final String topic, final MqttMessage message) throws Exception {
                 final String payload = new String(message.getPayload());
 
                 System.out.println("Received operation " + payload);
-                if (payload.startsWith("510")) {
+                if (payload.startsWith("{")) {
                     // execute the operation in another thread to allow the MQTT client to
                     // finish processing this message and acknowledge receipt to the server
+                	
+                	// I think this thread should update a counter to let people know it is 
+                	// alive and then publish every in an area of topics / data
+                	//
                     Executors.newSingleThreadScheduledExecutor().execute(new Runnable() {
                         public void run() {
-                            try {
-                                System.out.println("Simulating device restart...");
-                                client.publish("s/us", "501,c8y_Restart".getBytes(), 2, false);
-                                System.out.println("...restarting...");
-                                Thread.sleep(TimeUnit.SECONDS.toMillis(5));
-                                client.publish("s/us", "503,c8y_Restart".getBytes(), 2, false);
-                                System.out.println("...done...");
-                            } catch (MqttException e) {
-                                e.printStackTrace();
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
+                            System.out.println("Recieved topic:"+topic+", payload:"+payload);
+                            System.out.println("...done");
                         }
                     });
                 }
             }
         });
+    	
+    	
+    	
+        // listen for operations
+        client.subscribe("tele/tasmota_zbbridge_0576A9/SENSOR", new IMqttMessageListener() {
+            public void messageArrived (final String topic, final MqttMessage message) throws Exception {
+                final String payload = new String(message.getPayload());
+
+                //System.out.println("Received operation " + payload);
+                if (payload.contains("0xEE12")) {
+                    // execute the operation in another thread to allow the MQTT client to
+                    // finish processing this message and acknowledge receipt to the server
+                	
+                	// I think this thread should update a counter to let people know it is 
+                	// alive and then publish every in an area of topics / data
+                	//
+                    Executors.newSingleThreadScheduledExecutor().execute(new Runnable() {
+                        public void run() {
+                            System.out.println("Recieved topic:"+topic+", payload:"+payload);
+                            //System.out.println("...done");
+                        }
+                    });
+                }
+            }
+        });
+        
+        
+        
 
         // generate a random temperature (10º-20º) measurement and send it every 7 seconds
         Executors.newSingleThreadScheduledExecutor().scheduleWithFixedDelay(new Runnable() {
             public void run () {
                 try {
-                    int temp = (int) (Math.random() * 10 + 10);
-
-                    System.out.println("Sending temperature measurement (" + temp + "º) ...");
-                    client.publish("s/us", new MqttMessage(("211," + temp).getBytes()));
+                	// First update our poll counter to show we are alive :)
+                	//incrementPollCounter();
+                	pollCounter = pollCounter + 1;
+            	
+                	System.out.println("pollCounter:"+pollCounter);
+                    client.publish(pollCounterTopic.toString(), App.pollCounter.toString().getBytes(), 2, false);
+                    
+                    System.out.println("Publishing polling commands..");
+                    System.out.println("Tx topic:"+pollCommand[0]+", payload:"+ pollCommand[1]);
+                    client.publish(pollCommand[0], pollCommand[1].getBytes(), 2, false);
                 } catch (MqttException e) {
                     e.printStackTrace();
                 }
             }
-        }, 1, 7, TimeUnit.SECONDS);
+        }, 1, 600, TimeUnit.SECONDS);
     }
 
 

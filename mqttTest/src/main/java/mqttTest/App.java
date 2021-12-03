aec289018d9e1e55b15cc4e4b457bbd88ab5cdfa
Tony Walsh <kilnageer@gmail.com>
2021-12-03 23:03:19 +0000

    Completely changed code adding new file SimpleMqttClient.java

--- mqttTest/src/main/java/mqttTest/App.java
+++ mqttTest/src/main/java/mqttTest/App.java
@@ -11,17 +11,146 @@ import org.json.simple.parser.*;
 import org.eclipse.paho.client.mqttv3.*;
 import java.util.concurrent.*;
 
+
+
+
+/*
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttTopic;
+*/
+
+
 public class App {
+	
+	public static void main(String[] args) throws Exception {
+		SimpleMqttClient smc = new SimpleMqttClient();
+		smc.runClient();
+		
+		System.out.println("Program finished..");
+	}
+}
+
+
+
+
+
+
+/*
+
+package mqttTest;
+
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+import org.eclipse.paho.client.mqttv3.*;
+import java.util.concurrent.*;
+
+public class App implements MqttCallback {
 
 	static String[] pollCommand = { "cmnd/tasmota_zbbridge_0576A9/status", "" };
 	static Integer pollCounter = 0; // incremented every poll
 	static String pollCounterTopic = "Tola/status/pollCounter";
 	
+	public void messageArrived (final String topic, final MqttMessage message) throws Exception {
+	    final String payload = new String(message.getPayload());
+
+	    System.out.println("Received operation " + payload);
+	    if (payload.contains("Tasmota_ZbBridge")) {
+	        // execute the operation in another thread to allow the MQTT client to
+	        // finish processing this message and acknowledge receipt to the server
+	    	
+	    	
+	    	// {"Status":
+	    	//   {"Module":75,
+	    	//    "DeviceName":"Tasmota_ZbBridge",
+	    	//    "FriendlyName":["Tasmota_ZbBridge"],
+	    	//    "Topic":"tasmota_zbbridge_0576A9",
+	    	//    "ButtonTopic":"0",
+	    	//    "Power":0,
+	    	//    "PowerOnState":3,
+	    	//    "LedState":1,
+	    	//    "LedMask":"FFFF",
+	    	//    "SaveData":1,
+	    	//    "SaveState":1,
+	    	//    "SwitchTopic":"0",
+	    	//    "SwitchMode":[0,0,0,0,0,0,0,0],
+	    	//    "ButtonRetain":0,
+	    	//    "SwitchRetain":0,
+	    	//    "SensorRetain":0,
+	    	//    "PowerRetain":0,
+	    	//    "InfoRetain":0,
+	    	//    "StateRetain":0}
+	    	// }
+
+	        Object obj = new JSONParser().parse( payload );
+	          
+	        // typecasting obj to JSONObject
+	        JSONObject jo = (JSONObject) obj;
+	       
+	        
+	        try {
+	        	JSONObject jos = (JSONObject)jo.get("Status");
+	        	String DeviceName = (String) jos.get("DeviceName");
+	        	System.out.println("DeviceName is "+DeviceName);
+	        }
+	        catch (Exception e ) {
+	        	System.out.println("Exception #1 is "+e);
+	        }
+	    }
+	    
+	    if (payload.contains("ZbReceived")) {
+	    	// {"ZbReceived":
+	    	//   {"0xEE12":
+	    	//      {"Device":"0xEE12",
+	    	//       "Name":"\"TRV_Front_Bedroom\"",
+	    	//       "LocalTemperature":3,
+	    	//       "Endpoint":1,
+	    	//       "LinkQuality":47}
+	    	//   }
+	    	// }
+	    	// parsing file "JSONExample.json"
+	        Object obj = new JSONParser().parse( payload );
+	          
+	        // typecasting obj to JSONObject
+	        JSONObject jo = (JSONObject) obj;
+	            
+	        // getting fields of interest
+	        try {
+	        	JSONObject joz = (JSONObject)jo.get("ZbReceived");
+	        	JSONObject jod = (JSONObject)joz.get("0xEE12");
+	        	String localTemperature = (String) joz.get("LocalTemperature");
+	        	System.out.println("localTemperature for TRV 0xEE12 is "+localTemperature);
+	        }
+	        catch (Exception e ) {
+	        	System.out.println("Exception #2 is "+e);
+	        }
+	    }
+	    	
+	    // I think this thread should update a counter to let people know it is 
+		// alive and then publish every in an area of topics / data
+		//
+	    
+	}
+	    	
 	public static void incrementPollCounter() {
     	pollCounter = pollCounter + 1;
 	}
 	
     public static void main(String[] args) throws Exception {
+    		SimpleMqttClient smc = new App();
+    		smc.runClient();
+    	}
+    	
         int qos             = 2;
         String broker       = Secrets.getBroker();
         String clientId     = Secrets.getBroker();
@@ -36,129 +165,16 @@ public class App {
         final MqttConnectOptions options = new MqttConnectOptions();
         options.setUserName( username );
         options.setPassword( password.toCharArray() );
+        client.setCallback(this);
         client.connect(options);
         System.out.println("The device has been registered successfully!");
 
     	System.out.println("pollCounter:"+pollCounter);
     	
-        // listen for operations
-    	client.subscribe("stat/tasmota_zbbridge_0576A9/STATUS", new IMqttMessageListener() {
-            public void messageArrived (final String topic, final MqttMessage message) throws Exception {
-                final String payload = new String(message.getPayload());
-
-                System.out.println("Received operation " + payload);
-                if (payload.contains("Tasmota_ZbBridge")) {
-                    // execute the operation in another thread to allow the MQTT client to
-                    // finish processing this message and acknowledge receipt to the server
-                	
-                	
-                	// {"Status":
-                	//   {"Module":75,
-                	//    "DeviceName":"Tasmota_ZbBridge",
-                	//    "FriendlyName":["Tasmota_ZbBridge"],
-                	//    "Topic":"tasmota_zbbridge_0576A9",
-                	//    "ButtonTopic":"0",
-                	//    "Power":0,
-                	//    "PowerOnState":3,
-                	//    "LedState":1,
-                	//    "LedMask":"FFFF",
-                	//    "SaveData":1,
-                	//    "SaveState":1,
-                	//    "SwitchTopic":"0",
-                	//    "SwitchMode":[0,0,0,0,0,0,0,0],
-                	//    "ButtonRetain":0,
-                	//    "SwitchRetain":0,
-                	//    "SensorRetain":0,
-                	//    "PowerRetain":0,
-                	//    "InfoRetain":0,
-                	//    "StateRetain":0}
-                	// }
-
-                    Object obj = new JSONParser().parse( payload );
-                      
-                    // typecasting obj to JSONObject
-                    JSONObject jo = (JSONObject) obj;
-                   
-                    
-                    try {
-                    	JSONObject jos = (JSONObject)jo.get("Status");
-                    	String DeviceName = (String) jos.get("DeviceName");
-                    	System.out.println("DeviceName is "+DeviceName);
-                    }
-                    catch (Exception e ) {
-                    	System.out.println("Exception #1 is "+e);
-                    }
-                }
-                
-                if (payload.contains("ZbReceived")) {
-                	// {"ZbReceived":
-                	//   {"0xEE12":
-                	//      {"Device":"0xEE12",
-                	//       "Name":"\"TRV_Front_Bedroom\"",
-                	//       "LocalTemperature":3,
-                	//       "Endpoint":1,
-                	//       "LinkQuality":47}
-                	//   }
-                	// }
-                	// parsing file "JSONExample.json"
-                    Object obj = new JSONParser().parse( payload );
-                      
-                    // typecasting obj to JSONObject
-                    JSONObject jo = (JSONObject) obj;
-                        
-                    // getting fields of interest
-                    try {
-                    	JSONObject joz = (JSONObject)jo.get("ZbReceived");
-                    	JSONObject jod = (JSONObject)joz.get("0xEE12");
-                    	String localTemperature = (String) joz.get("LocalTemperature");
-                    	System.out.println("localTemperature for TRV 0xEE12 is "+localTemperature);
-                    }
-                    catch (Exception e ) {
-                    	System.out.println("Exception #2 is "+e);
-                    }
-                }
-                	
-                // I think this thread should update a counter to let people know it is 
-            	// alive and then publish every in an area of topics / data
-            	//
-                /*
-                Executors.newSingleThreadScheduledExecutor().execute(new Runnable() {
-                    public void run() {
-                        System.out.println("Recieved topic:"+topic+", payload:"+payload);
-                        System.out.println("...done");
-                    }
-                });
-                */
-                
-        };
-    	
-    	/*
     	
         // listen for operations
-        client.subscribe("tele/tasmota_zbbridge_0576A9/SENSOR", new IMqttMessageListener() {
-            public void messageArrived (final String topic, final MqttMessage message) throws Exception {
-                final String payload = new String(message.getPayload());
-
-                //System.out.println("Received operation " + payload);
-                if (payload.contains("{")) {
-                    // execute the operation in another thread to allow the MQTT client to
-                    // finish processing this message and acknowledge receipt to the server
-                	
-                	// I think this thread should update a counter to let people know it is 
-                	// alive and then publish every in an area of topics / data
-                	//
-                    Executors.newSingleThreadScheduledExecutor().execute(new Runnable() {
-                        public void run() {
-                            System.out.println("Recieved topic:"+topic+", payload:"+payload);
-                            //System.out.println("...done");
-                        }
-                    });
-                }
-            }
-            */
-    	
-        });
-        
+    	int subQoS = 0;
+    	client.subscribe("stat/tasmota_zbbridge_0576A9/STATUS", subQoS);
         
         
 
@@ -185,7 +201,7 @@ public class App {
 
 
  
-    /*
+    
     public class SimpleMqttCallBack implements MqttCallback {
 
     	  public void connectionLost(Throwable throwable) {
@@ -200,7 +216,10 @@ public class App {
     	    // not used in this example
     	  }
     	}
-    */
+    
+
+	
 }
         
+*/
 
---
+++ mqttTest/src/main/java/mqttTest/SimpleMqttClient.java
@@ -0,0 +1,171 @@
+package mqttTest;
+
+import org.eclipse.paho.client.mqttv3.*;
+
+public class SimpleMqttClient implements MqttCallback {
+
+	MqttClient myClient;
+	MqttConnectOptions connOpt;
+
+	//static final String BROKER_URL = "tcp://q.m2m.io:1883";
+	//static final String M2MIO_DOMAIN = "<Insert m2m.io domain here>";
+	//static final String M2MIO_STUFF = "things";
+	//static final String M2MIO_THING = "<Unique device ID>";
+	//static final String M2MIO_USERNAME = "<m2m.io username>";
+	//static final String M2MIO_PASSWORD_MD5 = "<m2m.io password (MD5 sum of password)>";
+
+	// the following two flags control whether this example is a publisher, a subscriber or both
+	static final Boolean subscriber = true;
+	static final Boolean publisher = true;
+
+	/**
+	 * 
+	 * connectionLost
+	 * This callback is invoked upon losing the MQTT connection.
+	 * 
+	 */
+	public void connectionLost(Throwable t) {
+		System.out.println("Connection lost!");
+		// code to reconnect to the broker would go here if desired
+	}
+
+	/**
+	 * 
+	 * deliveryComplete
+	 * This callback is invoked when a message published by this client
+	 * is successfully received by the broker.
+	 * 
+	 */
+	public void deliveryComplete(IMqttDeliveryToken token) {
+		try {
+			MqttMessage message = token.getMessage();
+			System.out.println("Pub complete" + new String(message.getPayload()));			
+		} catch (MqttException e) {
+			System.out.println("Exception #3 is "+e);
+		}
+
+	}
+
+	/**
+	 * 
+	 * messageArrived
+	 * This callback is invoked when a message is received on a subscribed topic.
+	 * 
+	 */
+	public void messageArrived(String topic, MqttMessage message) throws Exception {
+		System.out.println("-------------------------------------------------");
+		System.out.println("| Topic:" + topic);
+		System.out.println("| Message: " + new String(message.getPayload()));
+		System.out.println("-------------------------------------------------");
+	}
+
+	/**
+	 * 
+	 * MAIN
+	 * 
+	 */
+	//public static void main(String[] args) {
+	//	App smc = new App();
+	//	smc.runClient();
+	//}
+	
+	/**
+	 * 
+	 * runClient
+	 * The main functionality of this simple example.
+	 * Create a MQTT client, connect to broker, pub/sub, disconnect.
+	 * 
+	 */
+	public void runClient() {
+		// setup MQTT Client
+        int qos             = 2;
+        String broker       = Secrets.getBroker();
+        String clientId     = Secrets.getBroker();
+        String username		= Secrets.getUsername();
+        String password		= Secrets.getPassword();        
+
+        //final MqttClient client = new MqttClient( broker, clientId, null );
+        
+		//String clientID = M2MIO_THING;
+		//connOpt = new MqttConnectOptions();
+		connOpt = new MqttConnectOptions();
+		connOpt.setUserName( username );
+		connOpt.setPassword( password.toCharArray() );
+        //client.setCallback(this);
+        //client.connect(options);
+        
+		//connOpt.setCleanSession(true);
+		//connOpt.setKeepAliveInterval(30);
+		//connOpt.setUserName(M2MIO_USERNAME);
+		//connOpt.setPassword(M2MIO_PASSWORD_MD5.toCharArray());
+		
+		// Connect to Broker
+		try {
+			//myClient = new MqttClient(BROKER_URL, clientID);
+			myClient = new MqttClient( broker, clientId, null );
+			myClient.setCallback(this);
+			myClient.connect(connOpt);
+		} catch (MqttException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		System.out.println("Connected to " + broker);
+
+		// setup topic
+		// topics on m2m.io are in the form <domain>/<stuff>/<thing>
+		String myTopic = "stat/tasmota_zbbridge_0576A9/STATUS";
+		MqttTopic topic = myClient.getTopic(myTopic);
+
+		// subscribe to topic if subscriber
+		if (subscriber) {
+			try {
+				int subQoS = 0;
+				myClient.subscribe(myTopic, subQoS);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		// publish messages if publisher
+		if (publisher) {
+			for (int i=1; i<=1; i++) {
+		   		String pubMsg = "";
+		   		int pubQoS = 0;
+				MqttMessage message = new MqttMessage(pubMsg.getBytes());
+		    	message.setQos(pubQoS);
+		    	message.setRetained(false);
+		    	String topic2 = "cmnd/tasmota_zbbridge_0576A9/status";
+		    	MqttTopic pubTopic = myClient.getTopic(topic2);
+		    	
+
+		    	// Publish the message
+		    	System.out.println("Publishing to topic \"" + topic2 + "\" qos " + pubQoS);
+		    	MqttDeliveryToken token = null;
+		    	try {
+		    		// publish message to broker
+					token = pubTopic.publish(message);
+			    	// Wait until the message has been delivered to the broker
+					token.waitForCompletion();
+					Thread.sleep(100);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}			
+		}
+		
+		// disconnect
+		try {
+			// wait to ensure subscribed messages are delivered
+			if (subscriber) {
+				Thread.sleep(5000);
+			}
+			myClient.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+

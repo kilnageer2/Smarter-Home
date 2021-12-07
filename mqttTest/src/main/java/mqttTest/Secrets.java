a67f050f333bbe608c72b3ae332a600f4496a9de
Tony Walsh <kilnageer@gmail.com>
2021-12-07 10:18:48 +0000

    Added dummy Secrets.java filer for push to GitHub

--- mqttTest/src/main/java/mqttTest/Secrets.java
+++ mqttTest/src/main/java/mqttTest/Secrets.java
@@ -1,10 +1,10 @@
 package mqttTest;
 
 public class Secrets {
-	private final static String		broker = "tcp://192.168.3.6:1883";
-	private final static String		clientId = "javaClient_100";
-	private final static String		username = "tola";
-	private final static String		password = "2016TolaPark!";
+	private final static String		broker = "tcp://hostanme_or_i[p:1883"; // replace this!! 
+	private final static String		clientId = "uniqueclientname";  // replace this!! 
+	private final static String		username = "username"; // replace this!!
+	private final static String		password = "password!"; // replace this!!
 	
 	public static String getBroker() { return broker ; };
 	public static String getClientId() { return clientId ; };
